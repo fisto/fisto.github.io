f56632ef4b5cffcd1dec9584881d174c3fb9980d
fisto <cat.fisto@gmail.com>
2014-09-03 11:58:53 +0900

    update menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $(function() {
-  $('#top-menu').attr('data-content', $('#second-menu-body').html());
-  $('#top-menu').popover();
+  $('#top-menu-myself').attr('data-content', $('#top-menu-body-myself').html());
+  $('#top-menu-myself').popover();
+
+  $('#top-menu-social').attr('data-content', $('#top-menu-body-social').html());
+  $('#top-menu-social').popover();
 });
