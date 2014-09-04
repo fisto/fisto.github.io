5f5ad511f2a1095b4e04e8f2c572566e36d169a9
fisto <cat.fisto@gmail.com>
2014-09-04 11:34:09 +0900

    add content for profile

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 $(function() {
-  $('#top-menu-myself').attr('data-content', $('#top-menu-body-myself').html());
-  $('#top-menu-myself').popover();
+  /*
+  var csObj = new Object();
+  csObj.theme="dark";
+  $('.second-menu').mCustomScrollbar(csObj);
+  */
 
+  $('#top-menu-myself').attr('data-content', $('#top-menu-body-myself').html());
   $('#top-menu-social').attr('data-content', $('#top-menu-body-social').html());
-  $('#top-menu-social').popover();
+
+  if ((navigator.userAgent.indexOf('iPhone') > 0 && navigator.userAgent.indexOf('iPad') == -1) || navigator.userAgent.indexOf('iPod') > 0 || navigator.userAgent.indexOf('Android') > 0) {
+    $('#top-menu-myself').attr('data-trigger', 'hover');
+    $('#top-menu-social').attr('data-trigger', 'hover');
+  }
+
+  $('#top-menu-myself').popover({delay: { "show": 500, "hide": 100 }});
+  $('#top-menu-social').popover({delay: { "show": 500, "hide": 100 }});
 });
